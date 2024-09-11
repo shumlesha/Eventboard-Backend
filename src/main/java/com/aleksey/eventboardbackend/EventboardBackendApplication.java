95eacc9b69b2e84d84ee1d5100ca6a43fa5fbf82
Alexey Shumkov <shumleshas@gmail.com>
2024-09-11 18:26:07 +0700

    feat: initial

---
+++ src/main/java/com/aleksey/eventboardbackend/EventboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aleksey.eventboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventboardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aleksey/eventboardbackend/EventboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksey.eventboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventboardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
